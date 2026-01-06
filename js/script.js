6af5d0a4ee3b1e86bd304d697cf7e8591f7fb435
Aiden Chen <197520251+acstartup@users.noreply.github.com>
2026-01-06 10:17:28 -0500

    feat(HTML/CSS): implemented base user-side UI for sign up

---
+++ js/script.js
@@ -0,0 +1,20 @@
+/*
+Password Box
+Confirm Password Box
+Sign Up Button
+
+8 Letter Check (under Password Box - adds a check symbol when matched)
+Special Symbol (under Password Box - adds a check symbol when matched)
+Uppercase Check (under Password Box - adds a check symbol when matched)
+Lowercase Check (under Password Box - adds a check symbol when matched)
+Number Check (under Password Box - adds a check symbol when matched)
+Confirm Password (sign up button becomes clickable after confirmation)
+
+Alert on Sign Up success
+Alert when empty
+Alert when doesn't meet all requirementsd (dynamic based on what parameters aren't met)
+
+Optional: Reveal/Hide Password Button
+
+Clean Code!
+*/
